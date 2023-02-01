ba87bc5557c4dc84d2f1793ef28a7c0671c412ac
chencd97 <15999183+chencd97@users.noreply.github.com>
2023-02-01 10:01:54 +0800

    update

--- _init.sh
+++ _init.sh
@@ -4,6 +4,9 @@ service ssh restart
 #
 chmod -R 755 /home/docker
 #
-# 启动
-rm -rf /home/docker/debug.log
-java -jar -Xms64m -Xmx128m /home/docker/demo.jar 2>&1 | tee -a /home/docker/debug.log
+# 启动容器后的操作
+# rm -rf /home/docker/debug.log
+# java -jar -Xms64m -Xmx128m /home/docker/demo.jar 2>&1 | tee -a /home/docker/debug.log
+#
+# 为了让容器不停止执行以下命令(可自行修改)
+ping -i 30 127.0.0.1
--- build_tasks.sh
+++ build_tasks.sh
@@ -6,6 +6,16 @@ cp -rf /home/docker/sources.list /etc/apt/
 chmod 644 /etc/apt/sources.list
 rm -rf /home/docker/sources.list
 #
+# 恢复源文件
+# rm -rf /etc/apt/sources.list
+# mv /etc/apt/sources.list.bak /etc/apt/sources.list
+#
+# 复制ssh内容
+cp -rf /home/docker/sshd_config /etc/ssh/
+cp -rf /home/docker/ssh_config /etc/ssh/
+chmod 644 /etc/ssh/sshd_config && chmod 644 /etc/ssh/ssh_config
+rm -rf /home/docker/sshd_config && rm -rf /home/docker/ssh_config
+#
 # apt update
 apt-get update
 apt-get install -y openssh-server
@@ -21,17 +31,3 @@ apt-get install -y wget
 # 清理缓存
 apt-get autoremove -y
 apt-get autoclean -y
-#
-# 更新时区 中国用户填写[Asia/Shanghai], 表示亚洲/上海, 东八区
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-echo 'Asia/Shanghai' > /etc/timezone
-#
-# 恢复源文件
-# rm -rf /etc/apt/sources.list
-# mv /etc/apt/sources.list.bak /etc/apt/sources.list
-#
-# 复制ssh内容
-cp -rf /home/docker/sshd_config /etc/ssh/
-cp -rf /home/docker/ssh_config /etc/ssh/
-chmod 644 /etc/ssh/sshd_config && chmod 644 /etc/ssh/ssh_config
-rm -rf /home/docker/sshd_config && rm -rf /home/docker/ssh_config
