eb262388aaaeba65076550c053a4f7d7343ff794
chencd97 <15999183+chencd97@users.noreply.github.com>
2024-09-02 17:05:38 +0800

    update

--- _init.sh
+++ _init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 #
-service ssh restart
+# service ssh restart
 #
-chmod -R 755 /home/docker
+# chmod -R 755 /home/docker
 #
 # 启动容器后的操作
 # rm -rf /home/docker/debug.log
-# java -jar -Xms64m -Xmx128m /home/docker/demo.jar 2>&1 | tee -a /home/docker/debug.log
+java -jar \
+     -Xms64m \
+     -Xmx128m \
+     /home/docker/demo.jar 2>&1 | tee -a /home/docker/debug.log
 #
 # 为了让容器不停止执行以下命令(可自行修改)
-ping -i 30 127.0.0.1
+# ping -i 30 127.0.0.1
--- build_tasks.sh
+++ build_tasks.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 #
 # 修改 apt sources 源
-mv /etc/apt/sources.list /etc/apt/sources.list.bak
-cp -rf /home/docker/sources.list /etc/apt/
-chmod 644 /etc/apt/sources.list
-rm -rf /home/docker/sources.list
+# mv /etc/apt/sources.list /etc/apt/sources.list.bak
+# cp -rf /home/docker/sources.list /etc/apt/
+# chmod 644 /etc/apt/sources.list
+# rm -rf /home/docker/sources.list
 #
 # 恢复源文件
 # rm -rf /etc/apt/sources.list
 # mv /etc/apt/sources.list.bak /etc/apt/sources.list
-#
-# 复制ssh内容
-cp -rf /home/docker/sshd_config /etc/ssh/
-cp -rf /home/docker/ssh_config /etc/ssh/
-chmod 644 /etc/ssh/sshd_config && chmod 644 /etc/ssh/ssh_config
-rm -rf /home/docker/sshd_config && rm -rf /home/docker/ssh_config
 #
 # apt update
 apt-get update
